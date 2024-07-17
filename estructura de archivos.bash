02236f431da9db0fa503c619819e46d2b9cfa26a
FB <128921096+FreilinBetances@users.noreply.github.com>
2024-07-17 05:44:57 -0700

    Add files via upload

---
+++ estructura de archivos.bash
@@ -0,0 +1,13 @@
+npm create vite@latest my-app -- --template react
+cd my-app
+npm install
+
+my-app/
+├── public/
+│   └── index.html
+├── src/
+│   ├── App.jsx
+│   ├── main.jsx
+│   └── ... (otros archivos, sin incluir node_modules)
+├── package.json
+└── ... (otros archivos de configuración)
